10d4df2e12d6ed0f538624d125ad91c0fcfa875a
Oleh <olehbobeliak@gmail.com>
2021-06-24 15:17:34 +0300

    Added FizzBuzz class.
    Class has a constructor that takes 2 parameters (lower and upper bounds) and default constructor, which assigns lower and upper bounds to 1 and 100 respectively. Also class has method Play, which plays the game and prints results.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        readonly int _lowerBound;
+        readonly int _upperBound;
+
+        public FizzBuzz()
+        {
+            _lowerBound = 1;
+            _upperBound = 100;
+        }
+
+        public FizzBuzz(int v1, int v2)
+        {
+            if (v1 >= v2 || v1 <= 0) throw new ArgumentOutOfRangeException();
+            _lowerBound = v1;
+            _upperBound = v2;
+        }
+
+        public void Play()
+        {
+            for (int i = _lowerBound; i < _upperBound; i++)
+            {
+                if (i % 15 == 0)     Console.WriteLine("FizzBuzz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else                 Console.WriteLine(i);
+            }
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,13 +6,8 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 100; i++)
-            {
-                if (i % 15 == 0)     Console.WriteLine("FizzBuzz");
-                else if (i % 5 == 0) Console.WriteLine("Buzz");
-                else if (i % 3 == 0) Console.WriteLine("Fizz");
-                else                 Console.WriteLine(i);
-            }
+            FizzBuzz fb = new FizzBuzz(10, 20);
+            fb.Play();
         }
     }
 }
